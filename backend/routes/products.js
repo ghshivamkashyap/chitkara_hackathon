e26ef7d07ad33f2d7498a460a3edd9387343025c
SHIVAM KASHYAP <2130103@sliet.ac.in>
2024-01-16 18:52:45 +0530

    old code backend
    old code backend

---
+++ backend/routes/products.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const { getProductById, addProduct, genrateBiill, securityCheck } = require("../controllers/products");
+
+// Use a route parameter for 'pid'
+router.get("/getproductbyid/:pid", getProductById);
+router.get("/security/:billId", securityCheck);
+router.get("/addproduct", addProduct);
+router.post("/getbill", genrateBiill);
+
+
+module.exports = router;
