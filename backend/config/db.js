ca2a1b79657ba5c54382bae5e6cb7e81c5d8f287
SHIVAM KASHYAP <2130103@sliet.ac.in>
2024-01-16 19:02:43 +0530

    m

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,20 +1,3 @@
-// const mongoose = require("mongoose");
-// require("dotenv").config();
-
-// exports.connect = () => {
-//   mongoose
-//     .connect(process.env.MONGODB_URL, {
-//       dbName: process.env.DB_NAME,
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     })
-//     .then(() => console.log("db connected"))
-//     .catch((err) => {
-//       console.log("db connection failed");
-//       console.error(err);
-//       process.exit(1);
-//     });
-// };
 const mongoose = require("mongoose");
 require("dotenv").config();
 
